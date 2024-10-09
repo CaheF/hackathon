6e210f28195d1c0c0911513fa763a19b1a51ee6e
Cahe <cahefelipe10@gmail.com>
2024-10-08 22:00:01 -0300

    modificações no atendimento

--- triagem/relatorio.php
+++ triagem/relatorio.php
@@ -15,8 +15,8 @@
         <nav id="menu">
             <ul>
                 <li><a href="cadastro.php">Cadastrar paciente</a></li>
-                <li><a href="atendimento.php">Realizar triagem</a></li>
-                <li><a href="triagens.php">Ver triagens</a></li>
+                <li><a href="triagem.php">Realizar triagem</a></li>
+                <li><a href="relatorio.php">Ver triagens</a></li>
             </ul>
         </nav>
 
---
+++ triagem/triagem.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>atendimento</title>
+
+    <link rel="stylesheet" href="atendimento.css">
+</head>
+<body>
+<div id="container">
+        <nav id="menu">
+            <ul>
+                <li><a href="cadastro.php">Cadastrar paciente</a></li>
+                <li><a href="triagem.php">Realizar triagem</a></li>
+                <li><a href="relatorio.php">Ver triagens</a></li>
+            </ul>
+        </nav>
+    <div class="card">
+    <?php 
+        require('back/conectar.php');
+
+        $stmt = "SELECT idCadastro, nome, dataNasc, bairro, genero, trabalho, contato, documento, obs FROM cadastro"; 
+        $resultado = $conn->query($stmt);
+
+        if ($resultado->num_rows > 0) {
+            // Itera sobre os resultados
+            while ($row = $resultado->fetch_assoc()) {
+                echo "<p>ID: " . $row["idCadastro"] . "</p>" . "<br>";
+                echo "Nome: " . $row["nome"] . "<br>";
+                echo "Data de Nascimento: " . $row["dataNasc"] . "<br>";
+                echo "Bairro: " . $row["bairro"] . "<br>";
+                echo "Gênero: " . $row["genero"] . "<br>";
+                echo "Trabalho: " . $row["trabalho"] . "<br>";
+                echo "Contato: " . $row["contato"] . "<br>";
+                echo "Documento: " . $row["documento"] . "<br>";
+                echo "Observações: " . $row["obs"] . "<br><br>";
+            }
+        } else {
+            echo "Nenhum paciente encontrado.";
+        }
+
+        $conn->close();
+    ?>
+    </div>
+</div>
+</body>
+</html>
