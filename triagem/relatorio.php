462f90400b0e1d434aacf6a35da6a22d29e9f7c7
Diego <diegoolsv714@gmail.com>
2024-10-24 20:55:54 -0300

    ajuste telas

--- triagem/relatorio.php
+++ triagem/relatorio.php
@@ -14,73 +14,16 @@
     <nav id="menu">
             <ul>
                 <a href="./relatorio.php"><img src="image/aaa.png" alt="Logo do site"></a>
-                <li><a href="cadastro.php">Cadastrar paciente</a></li>
-                <li><a href="atendimento.php">Realizar triagem</a></li>
-                <li><a href="triagem.php">Triagens realizadas </a></li>
-                <li><a href="relatorio.php" class="active">Relatório </a></li>
+                <li><a href="cadastro.php">Evento</a></li>
             </ul>
         </nav>
 
         <section id="main-content">
-            <?php
 
-echo "<div class='relatorio'>";
+    <div class='relatorio'>
 
-include_once('back/conectar.php');
 
-echo "<h2>Relatório Geral</h2>";
-// Gênero mais cadastrado
-$sql = "SELECT genero, COUNT(*) AS quantidade FROM cadastro GROUP BY genero ORDER BY quantidade DESC LIMIT 1";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    echo "<div class='dados'>O gênero mais cadastrado é: <strong>" . $row['genero'] . "</strong> com <strong>" . $row['quantidade'] . "</strong> cadastros.</div>";
-} else {
-    echo "<div class='error'>Nenhum dado encontrado.</div>";
-}
-
-// Tipo sanguíneo mais cadastrado
-$sql = "SELECT tipoSangue, COUNT(*) AS quantidade FROM atendimento GROUP BY tipoSangue ORDER BY quantidade DESC LIMIT 1";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    $row = $result->fetch_assoc();
-    echo "<div class='dados'>O tipo sanguíneo mais cadastrado é: <strong>" . $row['tipoSangue'] . "</strong> com <strong>" . $row['quantidade'] . "</strong> cadastros.</div>";
-} else {
-    echo "<div class='error'>Nenhum dado encontrado.</div>";
-}
-
-// Total de registros
-$sql = "SELECT COUNT(*) AS total_registros FROM cadastro";
-$result = $conn->query($sql);
-
-if ($result) {
-    $row = $result->fetch_assoc();
-    echo "<div class='dados'>Total de pessoas registradas: <strong>" . $row['total_registros'] . "</strong></div>";
-} else {
-    echo "<div class='error'>Erro ao executar a consulta: " . $conn->error . "</div>";
-}
-
-// Peso médio
-$sql = "SELECT SUM(peso) AS total_peso, COUNT(peso) AS total_registros FROM atendimento";
-$result = $conn->query($sql);
-
-if ($result) {
-    $row = $result->fetch_assoc();
-    if ($row['total_registros'] > 0) {
-        $peso_medio = $row['total_peso'] / $row['total_registros'];
-        echo "<div class='dados'>O peso médio é: <strong>" . number_format($peso_medio, 2) . "</strong> kg</div>";
-    } else {
-        echo "<div class='error'>Nenhum registro de peso encontrado.</div>";
-    }
-} else {
-    echo "<div class='error'>Erro ao executar a consulta: " . $conn->error . "</div>";
-}
-
-echo "</div>";
-
-            ?>
+    <h2>Dashboard</h2>
 
         </section>
     </div>
