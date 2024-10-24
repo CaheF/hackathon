b7e66b577dc5c2b7a05ffab07039626caacb0154
Cahe <cahefelipe10@gmail.com>
2024-10-24 20:55:55 -0300

    cadastro alterado

--- triagem/conectar.php
+++ triagem/conectar.php
@@ -2,13 +2,7 @@
 $servername = "localhost:3308";
 $username = "root";
 $password = "etec2024";
-$dbname = "triagem";
-
-// casa diggas
-//$servername = "localhost:3306";
-//$username = "root";
-//$password = "etec2023";
-//$dbname = "triagem";
+$dbname = "saude";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
